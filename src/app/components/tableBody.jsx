6721ecffb50900c569730195d8dd0aa9746a8d54
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2023-12-18 02:26:22 +0300

    fix: change component table_body

--- src/app/components/tableBody.jsx
+++ src/app/components/tableBody.jsx
@@ -2,17 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 import { columns } from "../constants/columns";
 import _ from "lodash";
+import Badge from "./badge";
 
 const TableBody = ({ data }) => {
+    Object.values(columns).map((column) =>
+        console.log("column._id:", column._id)
+    );
     return (
         <tbody>
             {data.map((item) => (
                 <tr key={item._id}>
-                    {Object.keys(columns).map((column) => (
-                        <td key={column._id}>
-                            {_.get(item, columns[column].path)}
-                        </td>
-                    ))}
+                    {Object.keys(columns).map((key) => {
+                        if (key === "genres") {
+                            return (
+                                <td key={columns[key]._id}>
+                                    <Badge
+                                        {..._.get(
+                                            item,
+                                            columns[key].path.split(".")[0]
+                                        )}
+                                    />
+                                </td>
+                            );
+                        }
+                        return (
+                            <td key={columns[key]._id}>
+                                {_.get(item, columns[key].path)}
+                            </td>
+                        );
+                    })}
                 </tr>
             ))}
         </tbody>
