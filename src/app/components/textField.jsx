fedf2e1b39d5a8eb5b557c96fe1b666061fa6570
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-02-04 15:37:30 +0300

    fix: change text-field components

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
     return (
         <div className="mt-2">
             <label className="form-label" htmlFor={name}>
                 {label}
             </label>
-            <input
-                className={`form-control custom-input ${
-                    error ? "is-invalid" : ""
-                }`}
-                type={type}
-                id={name}
-                name={name}
-                placeholder={`enter ${name}`}
-                aria-label={label}
-                style={{
-                    color: "#fff",
-                    "::placeholder": {
+            <div className="input-group has-validation">
+                <input
+                    className={`form-control custom-input ${
+                        error ? "is-invalid" : ""
+                    }`}
+                    type={showPassword ? "text" : type}
+                    id={name}
+                    name={name}
+                    placeholder={`enter ${name}`}
+                    aria-label={label}
+                    style={{
                         color: "#fff",
-                        opacity: 0.7
-                    }
-                }}
-                value={value}
-                onChange={onChange}
-            />
-            {error && <div className="invalid-feedback mt-2">{error}</div>}
+                        "::placeholder": {
+                            color: "#fff",
+                            opacity: 0.7
+                        }
+                    }}
+                    value={value}
+                    onChange={onChange}
+                />
+                {type === "password" && (
+                    <button
+                        className="btn btn-outline-secondary"
+                        type="button"
+                        onClick={toggleShowPassword}
+                    >
+                        <i
+                            className={`bi bi-eye${
+                                showPassword ? "-slash" : ""
+                            }`}
+                        ></i>
+                    </button>
+                )}
+                {error && <div className="invalid-feedback mt-2">{error}</div>}
+            </div>
         </div>
     );
 };
